12c7e86240675cd3888e3f1e84024ea4eed91942
iWantToBeAProgrammer <juanstevenson10@gmail.com>
2025-11-18 03:02:54 +0700

    fix: bug on build vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
+    "/*": ["./node_modules/.prisma/client/**/*"],
+  },
 };
 
 export default nextConfig;
